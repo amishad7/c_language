24d575482dbb182f26aa4c04ac7225fd2dcdba80
amishad7 <amishad488@gmail.com>
2022-11-28 17:56:14 +0530

    c-files

---
+++ Ch2/2.2/1-4.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b;
+	clrscr();
+	printf("Enter a:");
+	scanf("%d",&a);
+	printf("Enter b:");
+	scanf("%d",&b);
+	printf("Divison of a & b vlaue = %d ",a/b);
+	getch();
+}
