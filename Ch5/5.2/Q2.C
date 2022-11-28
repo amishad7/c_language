9567380e12eb2c27c99a4f68018dcf5ca1454d3e
amishad7 <amishad488@gmail.com>
2022-11-28 18:09:43 +0530

    c-files

---
+++ Ch5/5.2/Q2.C
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b,c,d;
+	clrscr();
+	printf("Enter a:");
+	scanf("%d",&a);
+	printf("Enter b:");
+	scanf("%d",&b);
+	printf("Enter c:");
+	scanf("%d",&c);
+	printf("Enter d :");
+	scanf("%d",&d);
+	if(a>b)
+	{
+		if(a>c)
+		{
+			if(a>b)
+			{
+				printf("a is big");
+			}
+			else
+			{
+				printf("d is big");
+			}
+		}
+		else
+		{
+			printf("c is big");
+		}
+	}
+	else
+	{
+		if(b>c)
+		{
+			if(b>d)
+			{
+				printf("b is big");
+			}
+			else
+			{
+				printf("d is big");
+			}
+		}
+		else
+		{
+			if(c>d)
+			{
+				printf("c is big ");
+			}
+			else
+			{
+				printf("d is big");
+			}
+		}
+	}
+
+	getch();
+}
