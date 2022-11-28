c2a0dc52de0075bfc35434578cb54585652f21e6
amishad7 <amishad488@gmail.com>
2022-11-28 17:36:42 +0530

    C-files

---
+++ Ch2/1C.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("Name\t: Amisha desai\n");
+	printf("age\t: 18\n");
+	printf("school\t: Geetanjali school");
+	getch();
+}
---
+++ Ch2/a-3 (2).C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+   main()
+ {
+
+   clrscr();
+     printf("- - -\n");
+     printf("|   |\n");
+     printf("|   |\n");
+     printf("A   |\n");
+     printf("M   |\n");
+     printf("I   |\n");
+     printf("S   |\n");
+     printf("H   |\n");
+     printf("A   |\n");
+     printf("|   |\n");
+     printf("|   |\n");
+     printf("- - -\n");
+
+   getch();
+}
